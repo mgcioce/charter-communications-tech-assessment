d4c5cf65ab62328c6776bf41a4b965ee0fd93180
mgcioce <mgcioce@gmail.com>
2023-01-12 16:10:34 -0500

    updating to fix testing

--- tech-assessment/src/test/java/com/charter/techassessment/configuration/RewardsPointControllerTestConfiguration.java
+++ tech-assessment/src/test/java/com/charter/techassessment/configuration/RewardsPointControllerTestConfiguration.java
@@ -1,19 +1,10 @@
 package com.charter.techassessment.configuration;
 
-import com.charter.techassessment.controller.RewardsPointsController;
-import com.charter.techassessment.service.RewardsPointsService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
 
-@Configuration
+@TestConfiguration
 @Import(value = {RewardsPointsControllerConfiguration.class})
 public class RewardsPointControllerTestConfiguration {
 
-    @Bean
-    @Scope("singleton")
-    public RewardsPointsController rewardPointsController(RewardsPointsService rewardsPointsService) {
-        return new RewardsPointsController(rewardsPointsService);
-    }
 }
